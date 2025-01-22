2678c5e37c5b43dd4c8b4df58198602ba7fa855c
Lars Jaud <LarsJaud7@outlook.de>
2025-01-22 10:38:20 +0100

    Projectstart

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Lars Jaud on 22.01.25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
